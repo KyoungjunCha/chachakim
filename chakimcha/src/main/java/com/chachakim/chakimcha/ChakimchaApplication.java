502aff12db622376f7045692c0a7be8b754a6528
dijvajviav <ckrhksgh555@naver.com>
2024-05-01 16:01:03 +0900

    test 16:00:00

---
+++ chakimcha/src/main/java/com/chachakim/chakimcha/ChakimchaApplication.java
@@ -0,0 +1,13 @@
+package com.chachakim.chakimcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChakimchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChakimchaApplication.class, args);
+	}
+
+}
