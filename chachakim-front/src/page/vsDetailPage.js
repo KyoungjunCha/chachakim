2a5ae4c4769407b3b7d6a63569691b4dc3d73404
kim <loves7876@naver.com>
2024-05-07 00:59:58 +0900

    update

--- chachakim-front/src/page/vsDetailPage.js
+++ chachakim-front/src/page/vsDetailPage.js
@@ -2,18 +2,48 @@ import React from "react";
 import Menu from "../base/BaseMenu.js";
 import Header from "../base/BaseHeader.js";
 import PropType from "prop-types";
+import {useLocation} from "react-router-dom"
+import {VS_LIST} from "./ListDummy.js"
 
-const vsDetailPage = () => {
+const VsDetailPage = () => {
+
+  const location = useLocation();
+  const key = location.state.id;
+  const [vsList, setVSList] = React.useState(VS_LIST[key]);
+
   const renderBody = () => {
-    return;
+    console.log("vsList",vsList)
+    return(
+      <div style={{
+        position: "absolute",
+        marginTop: "100px",
+        top: "0px",
+        width: "85vw",
+        left: "50vw",
+        }}>
+        <div style={{display : "flex"}}>
+          <div style={{
+            border : "1px solid", 
+            width : "150px", 
+            textAlign : "center"
+            }}>VS이름</div>
+          <div style={{
+            border : "1px solid",
+            width : "400px"
+          }}
+          >{vsList["vs이름"]}</div>
+        </div>
+      </div>
+    );
   };
 
   return (
     <div>
+      {renderBody()}
       <Menu />
       <Header />
     </div>
   );
 };
 
-export default vsDetailPage;
+export default VsDetailPage;
