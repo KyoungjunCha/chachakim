b86b589f5b380c49fa7760027775c7bc3214720d
kim <loves7876@naver.com>
2024-05-08 01:07:45 +0900

    update vsDetail Page

--- chachakim-front/src/page/vsDetailPage.js
+++ chachakim-front/src/page/vsDetailPage.js
@@ -4,12 +4,22 @@ import Header from "../base/BaseHeader.js";
 import PropType from "prop-types";
 import {useLocation} from "react-router-dom"
 import {VS_LIST} from "./ListDummy.js"
+import "./vsDetailPage.css"
 
 const VsDetailPage = () => {
 
   const location = useLocation();
   const key = location.state.id;
   const [vsList, setVSList] = React.useState(VS_LIST[key]);
+  const [isDisplay, setIsDisplay] = React.useState(false);
+  const [frontPersent, setFrontPersent] = React.useState()
+  const [backPersent, setBackPersent] = React.useState()
+
+  const changeWidth = (e, place, width) => {
+    if(place = "front"){
+      setFrontPersent(e.target.value);
+    }
+  }
 
   const renderBody = () => {
 
@@ -80,13 +90,48 @@ const VsDetailPage = () => {
           </div>
         </div>
         <div style={{position : "absolute",  top : "300px"}}>
-          <div>
+          <div style={{display : "flex"}}>
             <button style={{
               border : "1px solid",
-              width : "300px"
-            }}>{vsList["vs이름"][0]}</button>
+              width : "200px"
+            }}
+              onClick={()=>{setIsDisplay(true); }}
+            >{vsList["vs이름"][0]}</button>
+            <div style={{fontSize : "30px", margin : "0 210px"}}>vs</div>
+            <button style={{
+              border : "1px solid",
+              width : "200px"
+            }}
+              onClick={()=>{setIsDisplay(true)}}
+            >{vsList["vs이름"][5]}
+            </button>
           </div>
-          
+        </div>
+        <div style={{position : "absolute", top : "350px", left : "-10px"}}>
+          {isDisplay?
+            <div style={{display : "flex"}}>
+              <div style={{backgroundColor : "skyblue", textAlign :"center", fontSize : ".8rem", fontWeight : "600", padding : ".5rem", borderRadius : ".5rem"}}> 
+                <div style={{display : "flex", justifyContent : "space-between"}}>
+                  <dvi>{vsList["vs이름"][0]}의 투표율</dvi>
+                  <div>72%</div>
+                </div>
+                <div className="vsDetail_persent_bar">
+                  <div className="vsDetail_persent_progress">
+                  </div>
+                </div>
+              </div>
+              <div style={{marginLeft : "435px", backgroundColor : "skyblue", textAlign :"center", fontSize : ".8rem", fontWeight : "600", padding : ".5rem", borderRadius : ".5rem"}}>
+                <div style={{display : "flex", justifyContent : "space-between"}}>
+                  <div>{vsList["vs이름"][5]}의 투표율</div>
+                  <div>28%</div>
+                </div>
+                <div className="vsDetail_persent_bar">
+                  <div className="vsDetail_persent_progress">
+                  </div>
+                </div>
+              </div>
+            </div>
+          :null}
         </div>
       </div>
     );
